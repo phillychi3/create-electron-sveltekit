7151643297eab449b1c7ad8c2857308d69a12d1f
philly <phillychi3@gmail.com>
2022-08-10 22:38:12 +0800

    add image

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # create sveltekit+electron
-
+![svelteimage](https://github.com/phillychi3/create-electron-sveltekit/blob/master/static/svelte.electron.png?raw=true)
 ## Developing
 
 ```bash
@@ -7,6 +7,7 @@ npm run electron-dev
 ```
 
 ## Build
+
 ```bash
 npm run electron-build
 
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,16 @@
+<script>
+	let count = 0;
+
+	function Click(){
+		count += 1;
+	}
+</script>
+<img src="/svelte.electron.png" width="200">
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button on:click={Click}>
+	Click me {count} times
+</button>
 
-<a href="/testpage">lol</a>
+<a href="/testpage">go to other page</a>
 
